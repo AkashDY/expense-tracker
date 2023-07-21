82fffce2d0d7164119edcadf52f9bf9a7cda287f
AkashDY <sky.deep.1997@gmail.com>
2023-07-21 15:51:30 +0530

    Initial commit

--- src/pages/CompleteProfile.js
+++ src/pages/CompleteProfile.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import AuthContext from "../components/store/auth-context";
 
@@ -9,6 +9,38 @@ const CompleteProfile = () => {
 
   const authCtx = useContext(AuthContext);
 
+  //----------------------------------------------------------
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      const url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCU5htJbsi7PPgASZ7_N5MjWr0esR_8l-Y";
+      const data = {
+        idToken: authCtx.token,
+      };
+
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const responseData = await response.json();
+        if (responseData.users && responseData.users.length > 0) {
+          const user = responseData.users[0];
+          setName(user.displayName || "");
+          setProfilePicture(user.photoUrl || null);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProfileData();
+  }, [authCtx.token]);
+
+  //-----------------------------------------------------------
   const submitHandler = async (event) => {
     event.preventDefault();
     const url =
@@ -33,7 +65,6 @@ const CompleteProfile = () => {
     }
     authCtx.completeProfile();
     history.push("/");
-    
   };
 
   return (
